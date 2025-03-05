843521a973c5d1978b83c9b2769e8e9e0ec3fac4
Ininnaja <inthirajail94@gmail.com>
2025-03-06 00:11:10 +0700

    เหลือfooter

--- php/src/student1.php
+++ php/src/student1.php
@@ -58,6 +58,7 @@
             </div>
         </div>
     </nav>
+    
     <div class="header-container">
         <span class="badge text-bg-primary header-content">
             <img src="assets/images/ce1.png" alt="Logo" class="logo">
@@ -95,7 +96,158 @@
         </div>
     </div>
 
-    
+    <div class="excel-container">
+        <div class="container text-center">
+            <div class="row align-items-center">
+                <div class="col">
+                    <div class="button-container">
+                        <a href="https://docs.google.com/spreadsheets/d/1nbbLxoq5Pp_ko5333pWofbNLt7jF5Olf/edit?usp=sharing&ouid=118158580503866060132&rtpof=true&sd=true" download>
+                            <img src="assets/images/excel.png" alt="Logo" class="logo">
+                        </a>
+                        <p>ไฟล์ Excel รายชื่อนักศึกษาชั้นปีที่ 1</p>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="button-container">
+                        <a href="https://docs.google.com/spreadsheets/d/1KUx4QOMh_cmpeJIASF_he4Muya32XoD6/edit?usp=sharing&ouid=118158580503866060132&rtpof=true&sd=true" download>
+                            <img src="assets/images/excel.png" alt="Logo" class="logo">
+                        </a>
+                        <p>ไฟล์ Excel รายชื่อนักศึกษาชั้นปีที่ 2</p>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="button-container">
+                        <a href="https://docs.google.com/spreadsheets/d/1eLiDsyzjsekUgrctE0KQ3_Mu5C9zuVrs/edit?usp=sharing&ouid=118158580503866060132&rtpof=true&sd=true" download>
+                            <img src="assets/images/excel.png" alt="Logo" class="logo">
+                        </a>
+                        <p>ไฟล์ Excel รายชื่อนักศึกษาชั้นปีที่ 3</p>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="button-container">
+                        <a href="https://docs.google.com/spreadsheets/d/1cHsYZFupMN47kerEKZGqgS1nYcKSXTf7/edit?usp=sharing&ouid=118158580503866060132&rtpof=true&sd=true" download>
+                            <img src="assets/images/excel.png" alt="Logo" class="logo">
+                        </a>
+                        <p>ไฟล์ Excel รายชื่อนักศึกษาชั้นปีที่ 4</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="welcome-box">
+            <h2 class="welcome-title">ชั้นปีที่ 1</h2>
+        </div>
+
+        <div class="container text-center">
+            <div class="row gy-4">
+                <div class="col">
+                    <div class="card">
+                        <div class="card-header">
+                            <h5 class="card-title">67200xxx</h5>
+                            <h5 class="card-text">นาย มิน ใจตี</h5>
+                        </div>
+                        <div class="card-body">
+                            <p><strong>ชื่อเล่น:</strong> มิน</p>
+                            <p><strong>วันเกิด:</strong> 01/01/2000</p>
+                            <p><strong>เบอร์โทร:</strong> 091-234-5678</p>
+                            <p><strong>ที่อยู่:</strong> 44/2</p>
+                            <p><strong>เบอร์โทรผู้ปกครอง:</strong> -</p>
+                        </div>
+                        <img src="assets/images/pro.jpg" class="card-img-top" alt="Profile Image">
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card">
+                        <div class="card-header">
+                            <h5 class="card-title">67200xxx</h5>
+                            <h5 class="card-text">นาย มิน ใจตี</h5>
+                        </div>
+                        <div class="card-body">
+                            <p><strong>ชื่อเล่น:</strong> มิน</p>
+                            <p><strong>วันเกิด:</strong> 01/01/2000</p>
+                            <p><strong>เบอร์โทร:</strong> 091-234-5678</p>
+                            <p><strong>ที่อยู่:</strong> 44/2</p>
+                            <p><strong>เบอร์โทรผู้ปกครอง:</strong> -</p>
+                        </div>
+                        <img src="assets/images/pro.jpg" class="card-img-top" alt="Profile Image">
+                    </div>
+                </div>
+            </div>
+            <div class="row gy-4">
+                <div class="col">
+                    <div class="card">
+                        <div class="card-header">
+                            <h5 class="card-title">67200xxx</h5>
+                            <h5 class="card-text">นาย มิน ใจตี</h5>
+                        </div>
+                        <div class="card-body">
+                            <p><strong>ชื่อเล่น:</strong> มิน</p>
+                            <p><strong>วันเกิด:</strong> 01/01/2000</p>
+                            <p><strong>เบอร์โทร:</strong> 091-234-5678</p>
+                            <p><strong>ที่อยู่:</strong> 44/2</p>
+                            <p><strong>เบอร์โทรผู้ปกครอง:</strong> -</p>
+                        </div>
+                        <img src="assets/images/pro.jpg" class="card-img-top" alt="Profile Image">
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card">
+                        <div class="card-header">
+                            <h5 class="card-title">67200xxx</h5>
+                            <h5 class="card-text">นาย มิน ใจตี</h5>
+                        </div>
+                        <div class="card-body">
+                            <p><strong>ชื่อเล่น:</strong> มิน</p>
+                            <p><strong>วันเกิด:</strong> 01/01/2000</p>
+                            <p><strong>เบอร์โทร:</strong> 091-234-5678</p>
+                            <p><strong>ที่อยู่:</strong> 44/2</p>
+                            <p><strong>เบอร์โทรผู้ปกครอง:</strong> -</p>
+                        </div>
+                        <img src="assets/images/pro.jpg" class="card-img-top" alt="Profile Image">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <footer class="footer" style="background-color: #203864;">
+        <div class="container p-4">
+            <!-- First row for links -->
+            <div class="row justify-content-center">
+                <!-- First column: Quick Links -->
+                <div class="col-md-5">
+                    <h5 class="text-white" style="font-size: 1.5rem;">แนะนำ</h5> <!-- เพิ่มขนาดฟอนต์ -->
+                    <ul class="list-unstyled">
+                        <li><a href="#!" class="text-custom" style="border-bottom: 2px solid white; display: inline-block; width: 80%; padding-bottom: 5px; font-size: 1.2rem;">นักศึกษา & บุคลากร CE</a></li>
+                        <li><a href="#!" class="text-custom" style="border-bottom: 2px solid white; display: inline-block; width: 80%; padding-bottom: 5px; font-size: 1.2rem;">ศิษย์เก่า CE & ITE</a></li>
+                        <li><a href="#!" class="text-custom" style="border-bottom: 2px solid white; display: inline-block; width: 80%; padding-bottom: 5px; font-size: 1.2rem;">ครุภัณฑ์การศึกษา CE</a></li>
+                        <li><a href="#!" class="text-custom" style="border-bottom: 2px solid white; display: inline-block; width: 80%; padding-bottom: 5px; font-size: 1.2rem;">ฝึกงาน & สหกิจศึกษา</a></li>
+                        <li><a href="#!" class="text-custom" style="border-bottom: 2px solid white; display: inline-block; width: 80%; padding-bottom: 5px; font-size: 1.2rem;">กิจกรรมหลักสูตร</a></li>
+                        <li><a href="#!" class="text-custom" style="border-bottom: 2px solid white; display: inline-block; width: 80%; padding-bottom: 5px; font-size: 1.2rem;">หลักสูตร & แผนการเรียน CE</a></li>
+                        <li><a href="#!" class="text-custom" style="border-bottom: 2px solid white; display: inline-block; width: 80%; padding-bottom: 5px; font-size: 1.2rem;">แต้มบุญ</a></li>
+                    </ul>
+                </div>
+
+                <!-- Second column: Contact Info -->
+                <div class="col-md-5">
+                    <h5 class="text-white" style="font-size: 1.5rem;">ติดต่อเรา</h5> <!-- เพิ่มขนาดฟอนต์ -->
+                    <ul class="list-unstyled" style="font-size: 1.2rem;"> <!-- เพิ่มขนาดฟอนต์ของข้อความในรายการ -->
+                        <li><i class="fas fa-map-marker-alt"></i> สถาบันเทคโนโลยีพระจอมเกล้าเจ้าคุณทหารลาดกระบัง</li>
+                        <li><i class="fas fa-map-marker-alt"></i>วิทยาเขตชุมพรเขตรอุดมศักดิ์ จังหวัดชุมพร</li>
+                        <li><i class="fas fa-map-marker-alt"></i>17/1 หมู่6 ต.ชุมโค อ.ปะทิว จ.ชุมพร 86160</li>
+                        <li class="phone" style="margin-top: 30px;">
+                            <img src="assets/images/f.png" alt="Icon"
+                                style="width: 34px; height: auto; margin-right: 10px;">
+                            <i class="fas fa-phone"></i><a href="https://www.facebook.com/ComEngKMITLPCC">Computer Engineering KMITL, PCC</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+
+
 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>

3416c0eb117fa4409889b5fc6b76923b930b1136
Ininnaja <inthirajail94@gmail.com>
2025-03-06 02:01:55 +0700

    ใส่หน้าloginเรีบยร้อย

--- php/src/index.php
+++ php/src/index.php
@@ -1,1 +1,59 @@
-<p>สร้างไฟล์ใครก็ทำได้เลยย</p>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="login.css">
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+Thai:wght@800&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+Thai:wght@100;300&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+</head>
+
+<body>
+    <div class="container">
+        <div class="left">
+            <img src="/assets/images/l.png" alt="Logo">
+            <div class="content">
+                <h1>LOGIN TO KMITL CE</h1>
+            </div>
+            <div class="login-box">
+            </div>
+            <div class="email-box">
+                <!-- กล่องแรกสำหรับ Email -->
+                <div class="input-box">
+                    <div class="icon">
+                        <i class="fas fa-envelope"></i> <!-- ใช้ไอคอนจดหมาย -->
+                    </div>
+                    <input type="email" placeholder="Email" />
+                </div>
+
+                <!-- กล่องที่สองสำหรับ Password -->
+                <div class="input-box">
+                    <div class="icon">
+                        <i class="fas fa-lock"></i> <!-- ใช้ไอคอนพาสเวิร์ด -->
+                    </div>
+                    <input type="password" placeholder="Password" />
+                </div>
+            </div>
+            <div class="for-get-pass">
+                <p>Forgot Password?</p>
+            </div>
+            <div class="Login-box">
+                <button class="Login">
+                    <h3>LOGIN</h3>
+                </button>
+            </div>
+        </div>
+        <div class="right">
+            <div class="contents">
+                <h4>Hello!</h4>
+                <h5>Enter your personal details</h5>
+                <h6>and start journey with us</h6>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
